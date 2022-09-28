cc8164756370d09229daaa73ac1ec6708c539866
MiaNara <lthutrang17@gmail.com>
2022-09-29 00:31:57 +0700

    change Blog.js

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -7,6 +7,11 @@ import Iconify from '../components/Iconify';
 import { BlogPostCard, BlogPostsSort, BlogPostsSearch } from '../sections/@dashboard/blog';
 // mock
 import POSTS from '../_mock/blog';
+import {
+  AppNewsUpdate,
+} from '../sections/@dashboard/app';
+import { faker } from '@faker-js/faker';
+import { useTheme } from '@mui/material/styles';
 
 // ----------------------------------------------------------------------
 
@@ -22,6 +27,7 @@ export default function Blog() {
   return (
     <Page title="Dashboard: Blog">
       <Container>
+      
         <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
           <Typography variant="h4" gutterBottom>
             Jobs
@@ -30,12 +36,23 @@ export default function Blog() {
             New Job
           </Button>
         </Stack>
-
+        
         <Stack mb={5} direction="row" alignItems="center" justifyContent="space-between">
           <BlogPostsSearch posts={POSTS} />
           <BlogPostsSort options={SORT_OPTIONS} />
         </Stack>
-
+        <Grid item xs={12} md={6} lg={8}>
+            <AppNewsUpdate
+              title="News Update"
+              list={[...Array(5)].map((_, index) => ({
+                id: faker.datatype.uuid(),
+                title: faker.name.jobTitle(),
+                description: faker.name.jobTitle(),
+                image: `/static/mock-images/covers/cover_${index + 1}.jpg`,
+                postedAt: faker.date.recent(),
+              }))}
+            />
+          </Grid>
         <Grid container spacing={3}>
           {POSTS.map((post, index) => (
             <BlogPostCard key={post.id} post={post} index={index} />
