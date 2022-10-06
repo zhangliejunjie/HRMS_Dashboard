5773075b3c539914cf5d59462786ce0d1cdd6793
zhangliejunjie <kiettltse161289@fpt.edu.vn>
2022-10-07 01:39:47 +0700

    Form by forkmik

--- src/sections/@dashboard/blog/CampaignCreateModal.js
+++ src/sections/@dashboard/blog/CampaignCreateModal.js
@@ -14,6 +14,7 @@ import { Menu, MenuItem, IconButton, ListItemIcon, ListItemText } from '@mui/mat
 import { Link as RouterLink } from 'react-router-dom';
 import Iconify from '../../../components/Iconify';
 import NewCampaignForm from 'src/sections/@dashboard/blog/NewCampaignForm.js';
+// Kiet uses  forkmik
 
 const style = {
     position: 'absolute',
--- src/sections/@dashboard/blog/NewCampaignForm.js
+++ src/sections/@dashboard/blog/NewCampaignForm.js
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 // @mui
-import { Stack, IconButton, InputAdornment, Typography } from '@mui/material';
+import { Stack, IconButton, InputAdornment, Typography, Select, MenuItem } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
 // components
 import Iconify from '../../../components/Iconify';
@@ -18,16 +18,20 @@ import TextField from '@mui/material/TextField';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-
+import { useFormik } from 'formik';
+import axios from 'axios';
+// api import
+import moment from 'moment/moment';
 // ----------------------------------------------------------------------
 
-export default function CampaignCreateModal() {
+export default function CampaignCreateForm() {
     const navigate = useNavigate();
-
     const [showPassword, setShowPassword] = useState(false);
 
     const [value1, setValue1] = React.useState(null);
     const [value2, setValue2] = React.useState(null);
+
+
 
 
     const RegisterSchema = Yup.object().shape({
@@ -35,8 +39,6 @@ export default function CampaignCreateModal() {
         description: Yup.string().required('Description required'),
         quantity: Yup.string().required('Quantity required'),
         salary: Yup.string().required('Salary required'),
-        // email: Yup.string().required('Last name required'),
-        // password: Yup.string().required('Password is required'),
     });
 
     const defaultValues = {
@@ -59,15 +61,65 @@ export default function CampaignCreateModal() {
     const onSubmit = async () => {
         navigate('/dashboard', { replace: true });
     };
+    // use forkmik
+    let date = Date.now();
+    const formik = useFormik({
+        initialValues: {
+            title: "",
+            description: '',
+            start_date: moment(date).format("yyyy-MM-DD"),
+            end_date: moment(date).format("yyyy-MM-DD"),
+            status: 'Processing',
+        },
+        validationSchema: Yup.object().shape({
+            title: Yup.string().required('Tên không được bỏ trống'),
+            description: Yup.string().required('Miêu tả không được bỏ trống'),
+            // start_date: Yup.date().required('Not null'),
+            // end_date: Yup.string().required('Not null'),
+            // start_date: Yup.date().min(new Date(), "Future date not allowed").typeError("Invalid Started date"),
+            // end_date: Yup.date().default(null)
+            // .when("started",
+            //     (started, yup) => started && yup.min(started, "End time cannot be before start time"))
+
+        }),
+        onSubmit: (value) => {
+            console.log(value);
+            axios.post('http://localhost:8000/api/campaign-add', {
+                'title': value.title,
+                'description': value.description,
+                'start_date': value.start_date,
+                'end_date': value.start_date,
+                'status': value.status
+            })
+                .then(res => {
+                    console.log(res);
+                    console.log(res.data);
+                    window.location.reload();
+                })
+                ;
+
+        }
+    })
+
 
     return (
-        <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+        <FormProvider methods={methods} onSubmit={formik.handleSubmit}>
             <Stack spacing={3}>
                 <Typography variant="h3"> Campaign name </Typography>
 
-                <RHFTextField name="title" label="Job Title" />
-                <RHFTextField name="description" label="Description" />
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <RHFTextField name="title" label="Job Title" id="title"
+                    value={formik.values.title}
+                    onChange={formik.handleChange}
+                    error={formik.touched.title && Boolean(formik.errors.title)}
+                    helperText={formik.touched.title && formik.errors.title} />
+                <RHFTextField name="description" label="Description"
+                    id="description"
+                    type="description"
+                    value={formik.values.description}
+                    onChange={formik.handleChange}
+                    error={formik.touched.description && Boolean(formik.errors.description)}
+                    helperText={formik.touched.description && formik.errors.description} />
+                {/* <LocalizationProvider dateAdapter={AdapterDayjs}>
                     <DatePicker
                         label="Start date"
                         value={value1}
@@ -75,17 +127,24 @@ export default function CampaignCreateModal() {
                             setValue1(newValue);
                         }}
                         renderInput={(params) => <TextField {...params} />}
+                        type="date"
+                        name="start_date"
+                        {...formik.getFieldProps('start_date')}
                     />
                     <DatePicker
                         label="End date"
                         value={value2}
-                        onChange={(newValue) => {
-                            setValue2(newValue);
-                        }}
-                        renderInput={(params) => <TextField {...params} />}
-                    />
-                </LocalizationProvider>
 
+                        renderInput={(params) => <TextField {...params} />}
+                        type="date"
+                        name="end_date"
+                        onChange={formik.handleChange}
+
+                    />
+                </LocalizationProvider> */}
+
+                <TextField fullWidth title='start date' type="date" name="start_date" onChange={formik.handleChange} defaultValue={moment(date).format("yyyy-MM-DD")} />
+                <TextField fullWidth title='end date' type="date" name="end_date" onChange={formik.handleChange} defaultValue={moment(date).format("yyyy-MM-DD")} />
 
                 {/* <RHFTextField name="title" label="Job Title" />
                 <RHFTextField name="description" label="Description" />
@@ -94,7 +153,7 @@ export default function CampaignCreateModal() {
                 <RHFTextField name="salary" label="Salary per month" type="number" /> */}
                 {/* <RHFTextField name="email" label="Email address" /> */}
 
-        {/* <RHFTextField
+                {/* <RHFTextField
           name="password"
           label="Password"
           type={showPassword ? 'text' : 'password'}
@@ -108,6 +167,19 @@ export default function CampaignCreateModal() {
             ),
           }}
         /> */}
+                <Select
+                    fullWidth
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    onChange={formik.handleChange}
+                    defaultValue="Processing"
+                    name='status'
+
+                >
+                    <MenuItem value={"Not started"}>Not started</MenuItem>
+                    <MenuItem value={'Processing'}>Processing</MenuItem>
+                    <MenuItem value={"Finished"}>Finished</MenuItem>
+                </Select>
 
                 <LoadingButton fullWidth size="large" type="submit" variant="contained" loading={isSubmitting}>
                     Create
